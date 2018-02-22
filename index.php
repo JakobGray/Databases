106675bd2bfd552dfad02fead7a44f8e685db591
JakobGray <jdg7sh@virginia.edu>
2018-02-21 21:19:18 -0500

    Trying out first files

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Session holding cookies for user info and article
+//$lifetime = 60 * 60 * 24 * 14;  // 2 weeks
+//session_set_cookie_params($lifetime);
+session_start();
+
+date_default_timezone_set("America/New_York");
+
+// Designate action variable
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'home';
+    }
+}
+
+switch ($action) {
+ 
+    case 'home':    // Displays all articles
+        include('views/home.php');
+        break;
+}
