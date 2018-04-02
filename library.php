0118e411808ab9d48f07fe9d1af3bf616975a3a7
kellylr <31316052+kellylr@users.noreply.github.com>
2018-04-02 14:24:17 -0400

    Update library.php

--- library.php
+++ library.php
@@ -1,6 +1,6 @@
 <?php
 $SERVER = 'stardock.cs.virginia.edu';
-$USERNAME = 'CS4750klr2xw';
-$PASSWORD = 'Dannyboy1!';
-$DATABASE = 'CS4750klr2xw';
+$USERNAME = 'username';
+$PASSWORD = 'password';
+$DATABASE = 'database';
 ?>
