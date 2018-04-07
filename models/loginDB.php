bcf1f85fb52028bbc11975584daa782767f18a7a
Jakob Gray <shyguy5512@yahoo.com>
2018-04-07 13:22:51 -0400

    ajax

--- models/loginDB.php
+++ models/loginDB.php
@@ -103,3 +103,16 @@ function get_scripts() {
   $query->closeCursor();
   return $result;
 }
+
+function save_results($username, $quizID, $score, $time) {
+  global $db;
+  $query = 'INSERT INTO score (username, quizID, score, duration)
+          VALUES (:username, :quizID, :score, :duration)';
+  $statement = $db->prepare($query);
+  $statement->bindValue(':username', $username);
+  $statement->bindValue(':quizID', $username);
+  $statement->bindValue(':score', $score);
+  $statement->bindValue(':duration', $duration);
+  $statement->execute();
+  $statement->closeCursor();
+}
--- models/saveResults.php
+++ models/saveResults.php
@@ -1,17 +1,5 @@
 <?php
-
-function save_results($username, $quizID, $score, $time) {
-  global $db;
-  $query = 'INSERT INTO score (username, quizID, score, duration)
-          VALUES (:username, :quizID, :score, :duration)';
-  $statement = $db->prepare($query);
-  $statement->bindValue(':username', $username);
-  $statement->bindValue(':quizID', $username);
-  $statement->bindValue(':score', $score);
-  $statement->bindValue(':duration', $duration);
-  $statement->execute();
-  $statement->closeCursor();
-}
+include('loginDB.php');
 
 $array = filter_input_array(INPUT_GET);
 print_r($array);
