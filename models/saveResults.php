2d31f3ebff56abfd1ae7444fb4206c6f77498398
Jakob Gray <shyguy5512@yahoo.com>
2018-04-07 12:59:22 -0400

    ajax

--- models/saveResults.php
+++ models/saveResults.php
@@ -6,3 +6,7 @@ $array = filter_input_array(INPUT_POST);
 // save_results($array['username'], $array['quizID'], $array['score'], $array['duration']);
 
 ?>
+<script type="text/javascript">
+    var all_questions = <?php echo json_encode($array); ?>;
+    console.log(JSON.stringify(all_questions, null, 2));
+</script>
