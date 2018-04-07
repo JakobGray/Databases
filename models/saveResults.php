247d8891337df8d0d477ab5893c249cac526d3db
Jakob Gray <shyguy5512@yahoo.com>
2018-04-07 13:01:02 -0400

    ajax

--- models/saveResults.php
+++ models/saveResults.php
@@ -2,11 +2,7 @@
 // require_once('/~jdg7sh/Databases/models/loginDB.php')
 // require_once('loginDB.php');
 $array = filter_input_array(INPUT_POST);
-// echo print_r($array);
+echo print_r($array);
 // save_results($array['username'], $array['quizID'], $array['score'], $array['duration']);
 
 ?>
-<script type="text/javascript">
-    var all_questions = <?php echo json_encode($array); ?>;
-    console.log(JSON.stringify(all_questions, null, 2));
-</script>
