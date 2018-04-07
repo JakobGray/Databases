e9942c7f72d2149f8f95d17e8dbb05358dae8227
Jakob Gray <shyguy5512@yahoo.com>
2018-04-07 12:56:03 -0400

    ajax

--- models/saveResults.php
+++ models/saveResults.php
@@ -1,5 +1,5 @@
 <?php
-require_once('/~jdg7sh/Databases/models/loginDB.php')
+// require_once('/~jdg7sh/Databases/models/loginDB.php')
 // require_once('loginDB.php');
 $array = filter_input_array(INPUT_GET);
 // echo print_r($array);
