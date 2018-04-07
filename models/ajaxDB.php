5da38f03ffcc42b7cafbbf00091e49cfb83fa1be
Jakob Gray <shyguy5512@yahoo.com>
2018-04-07 13:35:34 -0400

    Ajax

---
+++ models/ajaxDB.php
@@ -0,0 +1,15 @@
+<?php
+require('./database.php');
+
+function save_results($username, $quizID, $score, $time) {
+  global $db;
+  $query = 'INSERT INTO score (username, quizID, score, duration)
+          VALUES (:username, :quizID, :score, :duration)';
+  $statement = $db->prepare($query);
+  $statement->bindValue(':username', $username);
+  $statement->bindValue(':quizID', $username);
+  $statement->bindValue(':score', $score);
+  $statement->bindValue(':duration', $duration);
+  $statement->execute();
+  $statement->closeCursor();
+}
