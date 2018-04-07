9802f4e39aec4433afb1908903b37a8fa48af58d
Jakob Gray <shyguy5512@yahoo.com>
2018-04-07 12:55:22 -0400

    ajax

--- models/saveResults.php
+++ models/saveResults.php
@@ -2,7 +2,7 @@
 require_once('/~jdg7sh/Databases/models/loginDB.php')
 // require_once('loginDB.php');
 $array = filter_input_array(INPUT_GET);
-echo print_r($array);
-save_results($array['username'], $array['quizID'], $array['score'], $array['duration']);
+// echo print_r($array);
+// save_results($array['username'], $array['quizID'], $array['score'], $array['duration']);
 
 ?>
