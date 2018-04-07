1c93946298b7601e5f39f4c75d2b04e21592ea55
Jakob Gray <shyguy5512@yahoo.com>
2018-04-07 13:21:39 -0400

    ajax

--- models/saveResults.php
+++ models/saveResults.php
@@ -1,6 +1,18 @@
 <?php
-// require_once('/~jdg7sh/Databases/models/loginDB.php')
-// require_once('loginDB.php');
+
+function save_results($username, $quizID, $score, $time) {
+  global $db;
+  $query = 'INSERT INTO score (username, quizID, score, duration)
+          VALUES (:username, :quizID, :score, :duration)';
+  $statement = $db->prepare($query);
+  $statement->bindValue(':username', $username);
+  $statement->bindValue(':quizID', $username);
+  $statement->bindValue(':score', $score);
+  $statement->bindValue(':duration', $duration);
+  $statement->execute();
+  $statement->closeCursor();
+}
+
 $array = filter_input_array(INPUT_GET);
 print_r($array);
 save_results($array['username'], $array['quizID'], $array['score'], $array['duration']);
