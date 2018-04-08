7011896cdae03f02a83361ddc661add9e9300646
Jakob Gray <shyguy5512@yahoo.com>
2018-04-08 12:29:52 -0400

    A descriptive thing of whatever I just did

--- models/ajaxDB.php
+++ models/ajaxDB.php
@@ -12,13 +12,13 @@ $db = new PDO($dsn, $user, $password,
 array('charset'=>'utf8'));
 $db->query("SET CHARACTER SET utf8");
 
-function save_results($username, $quizID, $score, $time) {
+function save_results($username, $quizID, $score, $duration) {
   global $db;
   $query = 'INSERT INTO score (username, quizID, score, duration)
           VALUES (:username, :quizID, :score, :duration)';
   $statement = $db->prepare($query);
   $statement->bindValue(':username', $username);
-  $statement->bindValue(':quizID', $username);
+  $statement->bindValue(':quizID', $quizID);
   $statement->bindValue(':score', $score);
   $statement->bindValue(':duration', $duration);
   $statement->execute();
