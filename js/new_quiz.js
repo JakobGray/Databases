6cfa2689f8a0a36a823cd4586df68903e4628d6a
Jakob Gray <shyguy5512@yahoo.com>
2018-04-08 22:45:05 -0400

    Adding panels to quiz making page

--- js/new_quiz.js
+++ js/new_quiz.js
@@ -7,31 +7,23 @@ $(document).ready(function () {
         //Increment the unique index cause we are creating a new instance of the form
         hash++;
         //Clone the form and place it just before the button's <p>. Also give its id a unique index
-        var $newPanel = $template.clone().attr("id", "pheno_panel" + hash);
+        var $newPanel = $template.clone().attr("id", "panel" + hash);
         $newPanel.find(".collapse").removeClass("in");
-        $newPanel.find(".accordion-toggle").attr("href", "#" + hash)
-                .text(document.getElementById('phenotitle').value);
         $newPanel.find(".panel-collapse").attr("id", hash).addClass("collapse").removeClass("in");
-        $newPanel.find(".expandablephen").attr("id", 'pheno_value_detail_div' + hash);
-        $newPanel.find(".expandablepara").attr("id", 'exp_paradigm_detail_div' + hash);
+
         $("#accordion").append($newPanel.fadeIn());
         //Make the clone visible by changing CSS
-        $("#pheno_panel" + hash).css("display", "inline");
+        $("#panel" + hash).css("display", "inline");
         //For each input fields contained in the cloned form...
-        $("#pheno_panel" + hash + " :input").each(function () {
+        $("#panel" + hash + " :input").each(function () {
             //Modify the name attribute by adding the index number at the end of it
             $(this).attr("name", $(this).attr("name") + "[" + hash + "]");
             //Modify the id attribute by adding the index number at the end of it
             $(this).attr("id", $(this).attr("id") + "[" + hash + "]");
         });
-        //For each of the two buttons for showing detail
-        $("#pheno_panel" + hash + " :button").each(function () {
-            //Modify the toggle target by adding the index number at the end of it
-            $(this).attr("data-target", $(this).attr("data-target") + hash);
-        });
-        document.getElementById('phenoid' + "[" + hash + "]").value = document.getElementById('phenotitle').value;
     });
 });
+
 $(document).on('click', '.glyphicon-remove-circle', function () {
     $(this).parents('.panel').get(0).remove();
 });
--- views/add_tf_quiz.php
+++ views/add_tf_quiz.php
@@ -22,22 +22,25 @@
   <input type='submit' value='Submit' class="btn btn-success pull-left">
 </form> -->
 <div class="input-group col-md-6">
-    <input id='phenotitle' type="text" class="ui-widget col-md-12" placeholder="Phenoterm">
-    <span class="input-group-btn">
-        <button class="btn btn-default btn-add-panel" type="submit">
-            <i class="glyphicon glyphicon-plus"></i>
-        </button>
-    </span>
+    <button class="btn btn-default btn-add-panel" type="submit">
+        <i class="glyphicon glyphicon-plus"></i>
+    </button>
 </div>
 <br><br>
 <form action='.' class="ajax" method="post">
+  <label class="pull-left">Quiz Name:</label>
+  <input class="pull-left" type='text' name='quizname' maxlength="100" required>
+
+  <label class="pull-left">Quiz Topic:</label>
+  <input class="pull-left" type='text' name='topic' maxlength="300">
+  
     <div class="panel-group" id="accordion">
-        <div class="panel panel-default template" id="pheno_panel" style="display: none">
+        <div class="panel panel-default template" id="panel" style="display: none">
             <div class="panel-heading">
                 <div class="panel-heading"> <span class="glyphicon glyphicon-remove-circle pull-right "></span>
                     <h4 class="panel-title">
                         <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-                            Gene Expression
+                            Question
                         </a>
                     </h4>
                 </div>
@@ -46,159 +49,42 @@
             <div id="collapseThree" class="panel-collapse collapse">
                 <div class="panel-body">
                     <div class="form-group row">
-                        <label for="pheno_ontological_id" class="col-sm-3 col-form-label">Phenotypic ontological ID</label>
+                        <label for="prompt" class="col-sm-3 col-form-label">Prompt</label>
                         <div class="col-sm-6">
-                            <input type="text" class="form-control" name="pheno_ontological_id" placeholder="Phenotypic ontological ID">
+                            <input type="text" class="form-control" name="prompt" maxlength="300" placeholder="Prompt">
                         </div>
                     </div>
 
-                    <div class="form-group row">
-                        <label for="phenobase_extension_term" class="col-sm-3 col-form-label">PhenoBase extension term</label>
+                    <!-- <div class="form-group row">
+                        <label for="answer" class="col-sm-3 col-form-label">Anwer</label>
                         <div class="col-sm-6">
-                            <input type="text" class="form-control" name="phenobase_extension_term" placeholder="PhenoBase extension term">
+                          <select name='answer' class="pull-left">
+                            <option value="True">True</option>
+                            <option value="False">False</option>
+                          </select>
                         </div>
-                    </div>
-
-                    <div class="form-vertical col-md-12">
-                        <div class="col-md-6 inline">
-                            <div class="entry input-group" style="vertical-align: middle;">
-                                <label class="mr-sm-4" for="pheno_value">Pheno Value</label>
-                                <select class="custom-select mb-2 mr-sm-2" name="pheno_value">
-                                    <option selected>Choose...</option>
-                                    <option>Increased</option>
-                                    <option>Decreased</option>
-                                    <option>No Change</option>
-                                    <option>Abnormal</option>
-                                    <option>Ameliorated</option>
-                                    <option>Refractory</option>
-                                    <option>Side Effect</option>
-                                    <option>Restored</option>
-                                    <option>No Adverse Effect</option>
-                                    <option>Not Reported</option>
-                                </select>
-                                <span class="group-btn">
-                                    <button class="btn btn-default" type="button" data-toggle="collapse" data-target="#pheno_value_detail_div">
-                                        <i class="glyphicon glyphicon-plus"></i> Detail
-                                    </button>
-                                </span>
-                            </div>
-
-                            <div class="form-group collapse expandablephen" id="pheno_value_detail_div">
-                                <label for="pheno_value_detail">Pheno Value Detail:</label>
-                                <textarea class="form-control" rows="5" name="pheno_value_detail"></textarea>
-
-                            </div>
-
-                        </div>
-
-                        <div class="col-md-6 inline">
-                            <div class="entry input-group" >
-                                <label for="exp_paradigm" class="col-sm-8 form-label">Experimental Paradigm</label>
-                                <div style="display: inline-flex;">
-                                    <input type="text" class="form-control" name="exp_paradigm" placeholder="Experimental Paradigm">
-                                    <span class="input-group-btn">
-                                        <button class="btn btn-default" type="button" data-toggle="collapse" data-target="#exp_paradigm_detail_div">
-                                            <i class="glyphicon glyphicon-plus"></i> Detail
-                                        </button>
-                                    </span>
-                                </div>
-                            </div>
-
-                            <div class="form-group collapse expandablepara" id="exp_paradigm_detail_div" >
-                                <label for="exp_paradigm_detail">Experimental Paradigm Detail:</label>
-                                <textarea class="form-control" rows="5" name="exp_paradigm_detail"></textarea>
-                            </div>
-                            <br>
-                        </div>
-                    </div>
-
-
-
-                    <div class="form-group row">
-                        <label for="age" class="col-sm-3 col-form-label">Age at Testing</label>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="age" placeholder="Age at Testing">
-                        </div>
-                    </div>
+                    </div> -->
 
                     <fieldset class="form-inline row">
-                        <label for="gender" class="col-sm-3 col-form-label">Gender</label>
+                        <label for="answer" class="col-sm-3 col-form-label">Answer</label>
                         <div class="col-sm-6">
                             <label class="radio-inline">
-                                <input class="form-check-input" type="radio" name="gender" id="gridRadios1" value="female">
-                                Female
+                                <input class="form-check-input" type="radio" name="answer" id="gridRadios1" value="True">
+                                True
                             </label>
                             <label class="radio-inline">
-                                <input class="form-check-input" type="radio" name="gender" id="gridRadios2" value="male">
-                                Male
-                            </label>
-                            <label class="radio-inline">
-                                <input class="form-check-input" type="radio" name="gender" id="gridRadios3" value="?" checked>
-                                Unknown
+                                <input class="form-check-input" type="radio" name="answer" id="gridRadios2" value="False">
+                                False
                             </label>
                         </div>
                     </fieldset>
-
-                    <div class="form-inline row">
-                        <label class="col-sm-2 col-form-label" for="gp_symbol">Gene/Protein symbol</label>
-                        <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" name="gp_symbol" placeholder="Gene/Protein symbol">
-
-                        <label class="col-sm-2-right col-form-label" for="gp_symbol_id">Gene/Protein/GEO ID</label>
-                        <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-                            <input type="text" class="form-control" name="gp_symbol_id" placeholder="Gene/Protein/GEO ID">
-                        </div>
-                    </div>
-
-                    <div class="form-inline row" >
-                        <label class="col-sm-2 col-form-label" for="anatomy_region">Anatomical Region</label>
-                        <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" name="anatomy_region" placeholder="Anatomical Region">
-
-                        <label class="col-sm-2-right col-form-label" for="anatomy_region_id">Anatomical Region ID</label>
-                        <input type="text" class="form-control" name="anatomy_region_id" placeholder="Anatomical Region ID">
-
-                    </div>
-
-                    <div class="form-inline row">
-                        <label class="col-sm-2 col-form-label" for="cell_type">Cell Type</label>
-                        <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" name="cell_type" placeholder="Cell Type">
-
-                        <label class="col-sm-2-right col-form-label" for="cell_type_id">Cell Type ID</label>
-                        <input type="text" class="form-control" name="cell_type_id" placeholder="Cell Type ID">
-
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="pmid" class="col-sm-3 col-form-label">PMID</label>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="pmid" placeholder="PMID">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="figure" class="col-sm-3 col-form-label">Figure</label>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="figure" placeholder="Figure">
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="close_pheno_term" class="col-sm-3 col-form-label">Closest PhenoBase term</label>
-                        <div class="col-sm-6">
-                            <input type="text" class="form-control" name="close_pheno_term" placeholder="Closest PhenoBase term">
-                        </div>
-                    </div>
                 </div>
             </div>
-            <input type="hidden" name ="model_id" value="<?php echo $model_id ?>">
-            <input type="hidden" name ="pheno_id" id="phenoid" value="">
         </div>
     </div>
 
-    <input type="hidden" name="action" value="add_pheno">
-
+    <input type="hidden" name="action" value="new_tf_quiz">
     <input type="submit" value="submit" id="form_submit">
-
 </form>
-
 
 <?php include('./footer.php'); ?>
