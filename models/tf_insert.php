cfd829a983ba993544cd0cf7e3af8cdf72715549
Jakob Gray <shyguy5512@yahoo.com>
2018-04-09 00:41:38 -0400

    Fixed bug where indices don't start at 1

--- models/tf_insert.php
+++ models/tf_insert.php
@@ -4,12 +4,19 @@ require('./ajaxDB.php');
 $array = filter_input_array(INPUT_POST);
 echo print_r($array);
 
-// $quizname = $array['quizname'];
-// $topic = $array['topic'];
-// $quizID = create_new_tf_quiz($quizname, $topic);
+$quizname = $array['quizname'];
+$topic = $array['topic'];
+$quizID = create_new_tf_quiz($quizname, $topic);
 
 $keys = array_keys($array['prompt']);
-echo print_r($keys);
+// echo print_r($keys);
+foreach ($keys as $val) {
+  $prompt = $array['prompt'][$val];
+  $answer = $array['answer'][$val];
+  $questionID = create_new_tf_question($prompt, $answer);
+  link_question($quizID, $questionID);
+}
+
 // for ($i = 1; $i <= sizeof($_POST['prompt']); $i++) {
 //     $prompt = $array['prompt'][$i];
 //     $answer = $array['answer'][$i];
