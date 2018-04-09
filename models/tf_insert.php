bb3a53be1c9b1ff3684d264464be6745cf884ea7
Jakob Gray <shyguy5512@yahoo.com>
2018-04-09 00:26:48 -0400

    tf_insert.php for saving the quiz and questions

--- models/tf_insert.php
+++ models/tf_insert.php
@@ -4,15 +4,13 @@ require('./ajaxDB.php');
 $array = filter_input_array(INPUT_POST);
 echo print_r($array);
 
-// $quizname = filter_input(INPUT_POST, 'quizname');
-// $topic = filter_input(INPUT_POST, 'topic');
-// $quizID = create_new_tf_quiz($quizname, $topic);
-//
-// for ($i = 1; $i <= sizeof($_POST['prompt']); $i++) {
-//     $prompt = $array['prompt'][$i];
-//     $answer = $array['answer'][$i];
-//     $questionID = create_new_tf_question($prompt, $answer);
-//     link_question($quizID, $questionID);
-// }
+$quizname = $array['quizname'];
+$topic = $array['topic'];
+$quizID = create_new_tf_quiz($quizname, $topic);
 
-//// TODO: add in require
+for ($i = 1; $i <= sizeof($_POST['prompt']); $i++) {
+    $prompt = $array['prompt'][$i];
+    $answer = $array['answer'][$i];
+    $questionID = create_new_tf_question($prompt, $answer);
+    link_question($quizID, $questionID);
+}
