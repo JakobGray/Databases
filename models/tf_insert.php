1843ab0c1697fdb6d09760017bf9cfb6c267a197
Jakob Gray <shyguy5512@yahoo.com>
2018-04-09 00:37:24 -0400

    tf_insert.php for saving the quiz and questions

--- models/tf_insert.php
+++ models/tf_insert.php
@@ -4,13 +4,15 @@ require('./ajaxDB.php');
 $array = filter_input_array(INPUT_POST);
 echo print_r($array);
 
-$quizname = $array['quizname'];
-$topic = $array['topic'];
-$quizID = create_new_tf_quiz($quizname, $topic);
+// $quizname = $array['quizname'];
+// $topic = $array['topic'];
+// $quizID = create_new_tf_quiz($quizname, $topic);
 
-for ($i = 1; $i <= sizeof($_POST['prompt']); $i++) {
-    $prompt = $array['prompt'][$i];
-    $answer = $array['answer'][$i];
-    $questionID = create_new_tf_question($prompt, $answer);
-    link_question($quizID, $questionID);
-}
+$keys = array_keys($array);
+echo print_r($keys);
+// for ($i = 1; $i <= sizeof($_POST['prompt']); $i++) {
+//     $prompt = $array['prompt'][$i];
+//     $answer = $array['answer'][$i];
+//     $questionID = create_new_tf_question($prompt, $answer);
+//     link_question($quizID, $questionID);
+// }
