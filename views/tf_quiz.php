edea5e1e8bce0b1f542c75d143546876b16ae37c
Jakob Gray <shyguy5512@yahoo.com>
2018-04-09 11:06:34 -0400

    Records result if logged in

--- views/tf_quiz.php
+++ views/tf_quiz.php
@@ -1,9 +1,6 @@
 <?php
 include('views/header.php');
 $quizID = filter_input(INPUT_POST, 'quizID');
-if ($_SESSION['is_valid_user']) {
-  $username = $_SESSION['username'];
-}
 $questions = get_tf_questions_specific($quizID);
 ?>
 <link rel="stylesheet" href="./styles/tf_quiz.css">
@@ -11,7 +8,7 @@ $questions = get_tf_questions_specific($quizID);
 <script type="text/javascript">
     var all_questions = <?php echo json_encode($questions) ?>;
     var quizID = <?php echo $quizID ?>;
-    var playerID = <?php echo (isset($_SESSION['username'])) ? $_SESSION['username'] : ''; ?>;
+    var playerID = String(<?php echo (isset($_SESSION['username'])) ? $_SESSION['username'] : ''; ?>);
     console.log(JSON.stringify(all_questions, null, 2));
 </script>
 <script src="./js/tf_quiz.js"></script>
