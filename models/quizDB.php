02221f56176319364fc3a5f78e5e0a0959554ba7
Jakob Gray <shyguy5512@yahoo.com>
2018-04-14 12:47:59 -0400

    .

--- models/quizDB.php
+++ models/quizDB.php
@@ -4,7 +4,7 @@ function get_quizzes($type) {
   global $db;
   $query = $db->prepare("SELECT GID, name, topic FROM game
                           WHERE type = :type");
-  $statement->bindValue(':type', $type);
+  $query->bindValue(':type', $type);
   $query->execute();
   $result = $query->fetchAll(PDO::FETCH_ASSOC);
   $query->closeCursor();
