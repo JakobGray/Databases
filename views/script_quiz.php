34553b54eb9fc2bd6d1f62001b45c385f948021c
Jakob Gray <shyguy5512@yahoo.com>
2018-04-16 11:57:33 -0400

    debugging

--- views/script_quiz.php
+++ views/script_quiz.php
@@ -11,8 +11,8 @@ $scripts = get_scripts_specific($gameID);
 
     <script type="text/javascript">
         var all_scripts = <?php echo json_encode($scripts); ?>;
-        var text = all_scripts[1]['script_text'];
-        var answer = all_scripts[1]['answer'];
+        var text = all_scripts[0]['script_text'];
+        var answer = all_scripts[0]['answer'];
         console.log(JSON.stringify(all_scripts, null, 2));
     </script>
 
