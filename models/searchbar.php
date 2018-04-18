c14e7c530e7b560174c48836274fcdacb6ca66b9
Jakob Gray <shyguy5512@yahoo.com>
2018-04-18 13:43:10 -0400

    .

--- models/searchbar.php
+++ models/searchbar.php
@@ -5,11 +5,10 @@ $searchString = '%' . $_GET['searchName'] . '%';
 // filter_input(INPUT_POST, 'searchName', FILTER_SANITIZE_STRING) . '%';
 echo "Here am I";
 $results = get_searched_quizzes($searchString);
-//
-//
-//
+
+
 $keys = array_keys($results);
-echo print_r($results);
+// echo print_r($results);
 echo "<table border=1><th style='text-align: center'>Name</th><th>Topic</th><th>Play</th>\n";
 
 foreach ($keys as $val) {
@@ -17,7 +16,7 @@ foreach ($keys as $val) {
   $name = $results[$val]['name'];
   $topic = $results[$val]['topic'];
   // echo "Hello";
-  echo "<tr><td>$name</td><td>$topic</td><td><form action='.' method='POST'>;
+  echo "<tr><td>$name</td><td>$topic</td><td><form action='.' method='POST'>
         <input type='hidden' name='action' value='take_tf_quiz'>
         <input type='hidden' name='gameID' value='$GID'>
         <button type='submit' class='btn btn-large btn-primary'>Play</a>
