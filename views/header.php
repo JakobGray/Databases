e77a0cb4c707e3164158c2ca45456a2a99e1e1e5
klr2xw <klr2xw@virginia.edu>
2018-04-20 11:55:43 -0400

    test gitignore

--- views/header.php
+++ views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Quizzy</title>
+        <title>Quizzys</title>
 
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
         <meta name="viewport" content="width=device-width, initial-scale=1">
